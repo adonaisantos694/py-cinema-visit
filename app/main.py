7453942ff09fffe55ea5aece5d32c82fe7d65d5f
Adonai Soares <adonaisaores694@gmail.com>
2026-02-25 20:06:08 -0300

    Implement Resolution 2

--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
+from typing import List, Dict
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers, hall_number, cleaner, movie):
+def cinema_visit(
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str,
+    movie: str,
+) -> None:
     hall = CinemaHall(number=hall_number)
     cleaner_obj = Cleaner(name=cleaner)
 
     customer_objects = [
-        Customer(name=c["name"], food=c["food"])
-        for c in customers
+        Customer(name=customer["name"], food=customer["food"])
+        for customer in customers
     ]
 
     for customer in customer_objects:
         CinemaBar.sell_product(customer.food, customer)
 
-    hall.start_movie(movie)
-
-    for customer in customer_objects:
-        customer.watch_movie(movie)
-
-    hall.end_movie(movie)
-
-    cleaner_obj.clean_hall(hall_number)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaner_obj,
+    )
