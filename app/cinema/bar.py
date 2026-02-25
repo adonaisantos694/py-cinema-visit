518a7b43337900734c37d6ccb53caf7bf6a8a087
Adonai Soares <adonaisaores694@gmail.com>
2026-02-25 20:10:28 -0300

    Implement Resolution 4

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,4 +4,4 @@ from app.people.customer import Customer
 class CinemaBar:
     @staticmethod
     def sell_product(food: str, customer: Customer) -> None:
-        print(f'Cinema bar sold {food} to {customer.name}.')
+        print(f"Cinema bar sold {food} to {customer.name}.")
