7c56c357ca6239151e687cd46d4f529cf565f883
Adonai Soares <adonaisaores694@gmail.com>
2026-02-25 22:28:16 -0300

    Implement Resolution 8

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,6 +1,7 @@
-class CinemaBar:
+from app.people.customer import Customer
 
+
+class CinemaBar:
     @staticmethod
-    def sell_product(customer, product):
+    def sell_product(customer: Customer, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
-    
